789e79a1e70f8a5f3483621570584cb1df543a8f
quangthao2002 <quangthao22122002@gmail.com>
2023-09-13 08:34:44 +0700

    CommitTuan2

--- main.js
+++ main.js
@@ -20,4 +20,27 @@ var getAPI = function(){$.ajax({
 }
 getAPI();
 
+//  dung map render ra dữ liệu từ API về ==> lấy title và complete
+
+var getAPI2 = function(){$.ajax({
+    method: "GET",
+    url: urlBacon,
+    dataType: "json"
+})
+    .done(function (data) {
+        var obj = data.map(function (item) {
+            return {
+                "title": item.title,
+                "complete": item.completed,
+            }
+        })
+        console.log(obj);
+    })
+    .fail(function () {
+        alert("no good");
+    });
+}
+
+getAPI2();
+
 
